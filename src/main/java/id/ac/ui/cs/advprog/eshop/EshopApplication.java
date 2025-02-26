ce7a09772452945da2eb95bb5b5ef69f77fa8de2
Mabacore101 <valentinozhuo2@gmail.com>
2025-02-26 14:53:39 +0700

    Added component scan to main app.

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.eshop;
+import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "id.ac.ui.cs.advprog.eshop")
 public class EshopApplication {
 
     public static void main(String[] args) {
